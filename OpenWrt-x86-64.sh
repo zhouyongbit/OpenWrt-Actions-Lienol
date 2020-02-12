065f4f60c4a1c6b1aecd90a4afcfb2d093d1700c
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-02-12 15:28:49 +0800

    添加simple-obfs插件

--- OpenWrt-x86-64.sh
+++ OpenWrt-x86-64.sh
@@ -127,6 +127,7 @@ CONFIG_PACKAGE_haproxy=y
 CONFIG_PACKAGE_chinadns-ng=y
 CONFIG_PACKAGE_v2ray=y
 CONFIG_PACKAGE_v2ray-plugin=y
+CONFIG_PACKAGE_simple-obfs=y
 CONFIG_PACKAGE_trojan=y
 CONFIG_PACKAGE_brook=y
 CONFIG_PACKAGE_ipt2socks=y
